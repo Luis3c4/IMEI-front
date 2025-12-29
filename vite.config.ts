bbd2661f2a61b6bbe7f7ec0afe13f35638988a8f
luis3c4 <u20231587@utp.edu.pe>
2025-12-28 23:50:51 -0500

    fix: forzando tslib 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      'tslib': 'tslib/tslib.es6.js'
+      'tslib': path.resolve(__dirname, 'node_modules/tslib/tslib.es6.js')
     },
   },
 })
